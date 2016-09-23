790c69094bb8e795e533f6e7bd8eeb950050f94e
Kirk Abbott <kirkabbott1@gmail.com>
2016-09-23 09:50:40 -0400

    saves changes

--- madlib.py
+++ madlib.py
@@ -1,3 +1,3 @@
-name = raw_input("input name ")
-mario = raw_input("input your favorite school subject ")
+name = raw_input("What's your name ")
+mario = raw_input("what's your favorite Mariokart character? ")
 print "%s's favorite Mariokart character is %s" % (name, mario)
